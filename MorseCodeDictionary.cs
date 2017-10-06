097ca62b9673f583ffdccf29a585caddafdac5d3
pkdev-hell <pksrm27@gmail.com>
2017-10-05 22:41:41 -0700

    Add files via upload

---
+++ MorseCodeDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeEncoder
+{
+    public class MorseCodeDictionary
+    {
+        public Dictionary<char, String> MorseCodes;
+
+        public MorseCodeDictionary()
+        {
+            MorseCodes = new Dictionary<char, String>()
+            {
+                {'A' , ".-"},
+                {'B' , "-..."},
+                {'C' , "-.-."},
+                {'D' , "-.."},
+                {'E' , "."},
+                {'F' , "..-."},
+                {'G' , "--."},
+                {'H' , "...."},
+                {'I' , ".."},
+                {'J' , ".---"},
+                {'K' , "-.-"},
+                {'L' , ".-.."},
+                {'M' , "--"},
+                {'N' , "-."},
+                {'O' , "---"},
+                {'P' , ".--."},
+                {'Q' , "--.-"},
+                {'R' , ".-."},
+                {'S' , "..."},
+                {'T' , "-"},
+                {'U' , "..-"},
+                {'V' , "...-"},
+                {'W' , ".--"},
+                {'X' , "-..-"},
+                {'Y' , "-.--"},
+                {'Z' , "--.."},
+                {'0' , "-----"},
+                {'1' , ".----"},
+                {'2' , "..---"},
+                {'3' , "...--"},
+                {'4' , "....-"},
+                {'5' , "....."},
+                {'6' , "-...."},
+                {'7' , "--..."},
+                {'8' , "---.."},
+                {'9' , "----."},
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeEncoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter text to convert to Morse Code:: ");
+            string input = Console.ReadLine();
+
+            string output = ConvertToMorseCode(input);
+
+            Console.WriteLine("Morse Code: " + output);
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey(false);
+        }
+
+        private static string ConvertToMorseCode(string input)
+        {
+            char[] characters = input.ToUpper().ToCharArray();            
+            MorseCodeDictionary md = new MorseCodeDictionary();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in characters)
+            {
+                if (!c.Equals(' '))
+                {
+                    if (md.MorseCodes.ContainsKey(c))
+                        sb.Append(md.MorseCodes[c].ToString());
+                }
+                else
+                {
+                    sb.Append(" ");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
